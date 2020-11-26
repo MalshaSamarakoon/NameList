64f008086f7867cfdc5fc0d36ce501d8152b1ba1
MalshaSamarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2020-11-26 22:57:59 +0530

    developed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NameList from './components/NameList/NameList';
+
+function App() {
+  return( 
+  <div>
+        <NameList/>
+       
+  </div> 
+
+);
+
+}
+export default App;
