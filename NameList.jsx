4a1cf5b54d1feb7695cf10003e72bf24c1ccddd0
MalshaSamarakoon <73211958+MalshaSamarakoon@users.noreply.github.com>
2020-12-03 12:51:43 +0530

    random user

--- NameList.jsx
+++ NameList.jsx
@@ -1,21 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NameListItem from './NameListItem';
 
 function NameList(){
+
+    const [nameList, setNameList] = useState([
+        {id:1,
+        "name" :{"title": "mr","first": "brad","last": "gibson"},
+        "location": {"city": "kilcoole",},
+        "email": "brad.gibson@example.com",
+        "dob": {"date": "1993-07-20T09:44:18.674Z","age": 26,},
+        "picture": {"medium": "https://randomuser.me/api/portraits/med/men/75.jpg",},
+    },
+
+       {id:2,
+        "name":{"title":"Mr","first":"Dylan","last":"Margaret"},
+        "location":{"city":"Borden"},
+        "email":"dylan.margaret@example.com",
+        "dob":{"date":"1955-05-02T18:20:14.511Z","age":65,},
+        "picture":{"medium":"https://randomuser.me/api/portraits/med/men/4.jpg"},
+        },
+
+        {id:3,
+        "name":{"title":"Mr","first":"Luca","last":"Turner"},
+        "location":{"city":"Napier",},
+        "email":"luca.turner@example.com",
+        "dob":{"date":"1952-11-23T23:10:26.378Z","age":68,},
+        "picture":{"medium":"https://randomuser.me/api/portraits/med/men/54.jpg"},
+        }
+    ]);
+
+      
+
+    const nameListComponent=()=> {
+        return nameList.map((aName)=>{
+            return(
+              <NameListItem 
+                key={aName.id}
+                name={aName.name.first+' '+aName.name.last}
+                city={aName.location.city}
+                email={aName.email}
+                bday={aName.dob.date}
+                avatar={aName.picture.medium}
+                />
+           );
+        });
+    };
+
+    const addUserHandler = () =>{
+        const newUser ={
+            id:4,
+            "name":{"title":"Mr","first":"Cristina","last":"Thom"},
+            "location":{"city":"Borden"},
+            "email":"dylan.margaret@example.com",
+            "dob":{"date":"1955-05-02T18:20:14.511Z","age":65,},
+            "picture":{"medium":"https://randomuser.me/api/portraits/med/women/82.jpg"},
+        }
+
+            //setNameList((nameList)=>nameList.concat(newUser));   -----(1st tyep)
+
+            setNameList((nameList)=>[...nameList, newUser]);       //-----(2nd type ---- spread operator)
+    };   
+           
+                
+
         return(
-            <div>          
-                <h1>Name List</h1>
-     
-                <ul>
-                <NameListItem/> 
-                <NameListItem/> 
-                <NameListItem/> 
-                <NameListItem/> 
-                <NameListItem/>  
-                </ul> 
-            </div>            
+            <React.Fragment>  
+                <div className="container mt-4">
+                    
+                <ul className="list-group list-group-flush"> {nameListComponent()} </ul> 
+
+                <button className="btn btn-primary mt-4" onClick={addUserHandler}> Add Name </button>
+
+                </div>
+            </React.Fragment>              
         );
 }
 
 export default NameList;
-
+        
